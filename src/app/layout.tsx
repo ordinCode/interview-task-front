5a3e220945db9da3bdcf61afd4f9341feaefb861
윤성학 <ysh1346@naver.com>
2024-09-27 11:02:56 +0900

    recoil 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import LayoutRecoil from "./layout.recoil";
 
 export const metadata = {
   title: "myfair front pre-course",
   description: "todolist",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <LayoutRecoil>{children}</LayoutRecoil>
+      </body>
     </html>
   );
 }
